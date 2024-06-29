044ec36e2b29abf3a215217fd5b41b5e353f54c7
Lisbeth Cuenca Mamani <dioswachuka@gmail.com>
2024-06-29 03:00:21 -0400

    Parte 2

--- Parte-2/script.js
+++ Parte-2/script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=2e62f5b9eb1e6b54863abe247fcce108';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
